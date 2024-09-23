4c769c26cd9e5ff5dfb011567171b4d836a882d1
Salah Eddine Oukil <slhokli2002@gmail.com>
2024-09-23 13:39:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Deploy to Laravel Forge GitHub Action
+
+This GitHub Action deploys your site using the Laravel Forge API. It reads the deployment configuration from a `forge-deploy.yml` file, installs necessary dependencies, and executes the deployment script.
+
+## Inputs
+
+- `forge_api_token` (required): Laravel Forge API Token.
+- `deploy_config` (required): Base64-encoded content of `forge-deploy.yml`.
+
+## Usage
+
+To use this action, create a workflow file (e.g., `.github/workflows/deploy.yml`) in your repository with the following content:
+
+```yaml
+name: Deploy to Laravel Forge
+
+on:
+  push:
+    branches:
+      - main # Adjust to the branch that should trigger the deployment
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout the repository
+        uses: actions/checkout@v4
+
+      - name: Read forge-deploy.yml content
+        id: read_forge_deploy
+        run: |
+          deploy_config=$(cat forge-deploy.yml | base64 | tr -d '\n')
+          echo "deploy_config=$deploy_config" >> "$GITHUB_OUTPUT"
+        shell: bash
+        continue-on-error: false
+
+      - name: Deploy to Laravel Forge
+        uses: the-trybe/forge-deployment-scripts@main
+        with:
+          forge_api_token: ${{ secrets.FORGE_API_TOKEN }}
+          deploy_config: ${{ steps.read_forge_deploy.outputs.deploy_config }}
+```
+
+your repository should have a `forge-deploy.yml` file in the root of the repo.
+
+```yaml
+# forge-deploy.yml schema
+
+server_name: "your-server-name" # [Required] The name of your Laravel Forge server
+site_domain: "your-site-domain.com" # [Required] The domain of your site
+github_repository: "user/repo" # [Required] The GitHub repository URL for your site
+github_branch: "main" # [Optional] The branch to deploy (default: "main")
+project_type: "php" # [Optional] The type of the project (use "php" for laravel projects, otherwise don't include)
+php_version: "php83" # [Optional] PHP version to use (the version should be already installed in your server)
+build_commands: # [Optional] List of build commands to run during deployment
+  - npm install
+  - npm run build
+run_command: "npm run start" # [Optional] Command to run a process like a daemon
+environment: # [Optional] Environment variables for the site
+  APP_ENV: "production"
+  DB_HOST: "localhost"
+  DB_USER: "forge"
+  DB_PASSWORD: "yourpassword"
+aliases: # [Optional] Additional domain aliases
+  - "www.your-site-domain.com"
+nginx_template: "default" # [Optional] Nginx template to use (default: "default")
+```
+
+## Examples
+
+You can find example `forge-deploy.yml` files in the `examples` folder of this repository. These examples provide configurations for different types of projects.
+
+- [Laravel Example](examples/laravel/forge-deploy.yml)
+- [Next.js Example](examples/nextjs/forge-deploy.yml)
+
+Feel free to customize these example files to fit your deployment needs.
---
+++ deploy.py
@@ -0,0 +1,245 @@
+import logging
+import os
+import sys
+import time
+
+import requests
+import yaml
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+
+def deploy_site():
+    forge_uri = "https://forge.laravel.com/api/v1"
+    forge_api_token = os.getenv("FORGE_API_TOKEN")
+    if forge_api_token is None:
+        raise Exception("FORGE_API_TOKEN is not set")
+
+    new_site_created = False
+
+    with open("forge-deploy.yml", "r") as file:
+        data = yaml.safe_load(file)
+        #TODO: validate data
+        config ={
+            "server_name": data["server_name"],
+            "site_domain": data["site_domain"],
+            "github_repository": data["github_repository"],
+            "github_branch": data.get("github_branch", "main"),
+            "project_type": data.get("project_type", "html"),
+            "php_version": data.get("php_version",None),
+            "build_commands": data.get("build_commands", []),
+            "run_command": data.get("run_command",None),
+            "environment": data.get("environment", {}),
+            "aliases": data.get("aliases", []),
+            "nginx_template": data.get("nginx_template", "default"),
+        }
+
+    headers = {
+        "Authorization": f"Bearer {forge_api_token}",
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    }
+
+    response = requests.get(f"{forge_uri}/servers", headers=headers) 
+    response.raise_for_status()
+    server_id = next(
+        (
+            server["id"]
+            for server in response.json()[
+                "servers"
+            ]
+            if server["name"] == config["server_name"]
+        ),
+        None,
+    )
+    if not server_id:
+        raise Exception("Server not found")
+
+    response = requests.get(
+            f"{forge_uri}/servers/{server_id}/sites", headers=headers
+        )
+    response.raise_for_status()
+    site = next(
+        (site for site in response.json()["sites"] if site["name"] == config["site_domain"]),None
+    )
+
+    #create site
+    if not site:
+        response = requests.get(
+            f"{forge_uri}/servers/{server_id}/nginx/templates", headers=headers
+        )
+        response.raise_for_status()
+        nginx_templates = response.json()["templates"]
+        nginx_template_id = next(
+            (item["id"] for item in nginx_templates if item["name"] == config["nginx_template"]), None
+        )
+
+        if not nginx_template_id:
+            raise Exception("Nginx template not found")
+
+        create_site_payload = {
+            "domain": config["site_domain"],
+            "project_type": config["project_type"],
+            "aliases": config["aliases"],
+            "isolated": False,
+            "nginx_template": nginx_template_id,
+        }
+
+        # create site
+        logger.info("Creating site...")
+        response = requests.post(
+            f"{forge_uri}/servers/{server_id}/sites",
+            json=create_site_payload,
+            headers=headers,
+        )
+        response.raise_for_status()
+        site = response.json()["site"]
+
+        while site["status"] != "installed":
+            time.sleep(1)
+            site = requests.get(
+                f"{forge_uri}/servers/{server_id}/sites/{site['id']}", headers=headers
+            ).json()["site"]
+
+        
+        new_site_created = True
+        logger.info("Site created successfully")
+
+
+    site_id = site["id"]
+
+    # add repository
+    if site["repository"] != config["github_repository"]:
+        logger.info("adding repository...")
+        response = requests.post(
+            f"{forge_uri}/servers/{server_id}/sites/{site_id}/git",
+            headers=headers,
+            json={
+                "provider": "github",
+                "repository": config["github_repository"],
+                "branch": config["github_branch"],
+                "composer": True if config["project_type"] == "php" else False,
+            },
+        )
+        response.raise_for_status()
+        site = response.json()["site"]
+
+        while site["repository_status"] != "installed":
+            time.sleep(2)
+            site = requests.get(
+                f"{forge_uri}/servers/{server_id}/sites/{site_id}", headers=headers
+            ).json()["site"]
+
+        logger.info("Repository added successfully")
+
+    # create daemon
+    daemon_id = None
+    if config["run_command"]:
+        response = requests.get(
+                    f"{forge_uri}/servers/{server_id}/daemons", headers=headers
+                )
+        response.raise_for_status()
+        daemon_id = next(
+            (
+                daemon["id"]
+                for daemon in response.json()["daemons"]
+                if daemon["command"] == config["run_command"] and daemon["directory"] == f"/home/forge/{config["site_domain"]}"
+            ),
+            None,
+        )
+        if not daemon_id:
+            response = requests.post(
+                f"{forge_uri}/servers/{server_id}/daemons",
+                headers=headers,
+                json={
+                    "command": config["run_command"],
+                    "user": "forge",
+                    "directory": f"/home/forge/{config["site_domain"]}",
+                    "startsecs": 1,
+                },
+            )
+            response.raise_for_status()
+            daemon_id = response.json()["daemon"]["id"]
+
+    # deployment script
+    # if build_commands not provided, the default deployment script generated by forge is kept
+    if len(config["build_commands"]) > 0:
+        deployment_script =  f"""#generated by deployment script don't modify
+cd /home/forge/{config["site_domain"]}
+git pull origin $FORGE_SITE_BRANCH
+"""
+        for cmd in config["build_commands"]:
+            deployment_script += f"{cmd}\n"
+        if daemon_id:
+            deployment_script += f"sudo -S supervisorctl restart daemon-{daemon_id}:*\n"
+
+        response = requests.put(
+            f"{forge_uri}/servers/{server_id}/sites/{site_id}/deployment/script",
+            headers=headers,
+            json={
+                "content": deployment_script,
+                "auto_source": True if len(config["environment"])>0 else False,
+            },  # to make .env available for the build
+        )
+        response.raise_for_status()
+    
+
+    # set env
+    env =""
+    for key, value in config["environment"].items():
+        env += f"{key}=\"{value}\"\n"
+    if len(env) > 0:    
+        response = requests.put(
+            f"{forge_uri}/servers/{server_id}/sites/{site_id}/env",
+            headers=headers,
+            json={
+                "content": env,
+            },
+        )
+        response.raise_for_status()
+
+    #certificate
+    if new_site_created:
+        response = requests.post(
+            f"{forge_uri}/servers/{server_id}/sites/{site_id}/certificates/letsencrypt",
+            headers=headers,
+            json={"domains": [config["site_domain"], *config["aliases"]]},
+        )
+        response.raise_for_status()
+
+    # deploy site
+    logger.info("Deploying site...")
+    response = requests.post(
+        f"{forge_uri}/servers/{server_id}/sites/{site_id}/deployment/deploy",
+        headers=headers,
+    )
+    response.raise_for_status() 
+    site = response.json()["site"]
+
+    while site["deployment_status"] != None:
+        time.sleep(2)
+        site = requests.get(
+            f"{forge_uri}/servers/{server_id}/sites/{site_id}", headers=headers
+        ).json()["site"]
+
+    deployment = requests.get(
+            f"{forge_uri}/servers/{server_id}/sites/{site_id}/deployment-history", headers=headers
+        ).json()["deployments"][0]
+    if (deployment["status"] == "failed"):
+        raise Exception("Deployment failed")
+    logger.info("Site deployed successfully")
+
+
+if __name__ == "__main__":
+    try:
+        deploy_site()
+    except requests.exceptions.HTTPError as http_err:
+        logger.error("HTTP error occurred: %s", http_err, exc_info=True)
+        sys.exit(1)
+    except Exception as err:
+        logger.error(f"An error occurred: %s", err, exc_info=True)
+        sys.exit(1)
